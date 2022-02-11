de75df2afca899ef2fb0587494d73af6791a2616
peterjidamva <jidamvap@gmail.com>
2022-02-11 20:13:32 +0300

    first hands on solidity

---
+++ contract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+//it specifies the version of compiler to use
+pragma solidity >=0.4.0 <0.9.0;
+
+
+// contract is like a class and helloWorld is a class name 
+contract HelloWorld {
+    string public greet = "Hello World!";
+}
